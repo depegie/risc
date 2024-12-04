5a80d9c840584f60614a25e6e4a8a46f8f6a6f08
Daniel <27815569+depegie@users.noreply.github.com>
2024-12-04 22:00:54 +0100

    Dodano dekoder instrukcji

--- include/defines.svh
+++ include/defines.svh
@@ -6,4 +6,14 @@
 `define WORD_SIZE_B    4
 `define WORD_SIZE      8*`WORD_SIZE_B
 
+`define INSTR_OPCODE     instr[6:0]
+`define INSTR_RD         instr[11:7]
+`define INSTR_FUNCT3     instr[14:12]
+`define INSTR_RS1        instr[19:15]
+`define INSTR_RS2        instr[24:20]
+`define INSTR_FUNCT7     instr[31:25]
+`define INSTR_I_TYPE_IMM instr[31:20]
+`define INSTR_S_TYPE_IMM {instr[31:25], instr[11:7]} 
+`define INSTR_J_TYPE_IMM instr[31:12]
+
 `endif
--- testbench/tb_core.sv
+++ testbench/tb_core.sv
@@ -13,9 +13,10 @@ module tb_core();
     logic [`WORD_SIZE-1 : 0] tb_wb_rdata;
     logic                    tb_wb_ack;
 
-    localparam [6:0] SW_INSTR_OPCODE  = 7'b0100011;
-    localparam [6:0] LW_INSTR_OPCODE  = 7'b0000011;
-    localparam [6:0] ADD_INSTR_OPCODE = 7'b0110011;
+    localparam [6 : 0] SW_INSTR_OPCODE   = 7'b0100011;
+    localparam [6 : 0] LW_INSTR_OPCODE   = 7'b0000011;
+    localparam [6 : 0] ADD_INSTR_OPCODE  = 7'b0110011;
+    localparam [6 : 0] ADDI_INSTR_OPCODE = 7'b0010011;
 
     always #(CLK_PERIOD/2) tb_clk = !tb_clk;
 
@@ -25,9 +26,9 @@ module tb_core();
 
         lw(5'b00000, 5'b00001, 12'b0000_0111_1111);
         read_ack(32'hfffff000);
-        lw(5'b00000, 5'b00010, 12'b0000_0111_1110);
-        read_ack(32'h00000fff);
-        add(5'b00001, 5'b00010, 5'b00011);
+        // lw(5'b00000, 5'b00010, 12'b0000_0111_1110);
+        // read_ack(32'h00000fff);
+        addi(5'b00001, 5'b00010, 12'hfff);
 
         // $finish();
     end
@@ -41,6 +42,7 @@ module tb_core();
 
     task deassert_reset();
         #(2*CLK_PERIOD) tb_rst = 1'b0;
+        @(posedge tb_clk) #1;
         @(posedge tb_clk) #1;
     endtask
 
@@ -64,6 +66,15 @@ module tb_core();
 
     task add(bit[4:0] rs1, bit[4:0] rs2, bit[4:0] rd);
         tb_wb_rdata = {7'b0000000, rs2, rs1, 3'b000, rd, ADD_INSTR_OPCODE};
+        tb_wb_ack = 1'b1;
+        @(posedge tb_clk) #1;
+        tb_wb_rdata = 'b0;
+        tb_wb_ack = 1'b0;
+        @(posedge tb_clk) #1;
+    endtask
+
+    task addi(bit[4:0] rs1, bit[4:0] rd, bit[11:0] imm);
+        tb_wb_rdata = {imm, rs1, 3'b000, rd, ADDI_INSTR_OPCODE};
         tb_wb_ack = 1'b1;
         @(posedge tb_clk) #1;
         tb_wb_rdata = 'b0;
