cfffba8a9b163b0d15fd1be02cb5d33e9246e095
michalpapiernik <85085251+michalpapiernik@users.noreply.github.com>
2024-12-12 20:02:07 +0100

    Dodany koder i dekoder ASCI/hex

--- modules/ascii_command_decoder.sv
+++ modules/ascii_command_decoder.sv
@@ -23,8 +23,13 @@ module ascii_command_decoder (
     typedef enum logic [2:0] {
         IDLE,
         CHECK_CMD,
-        PARSE_ADDR,
-        PARSE_DATA,
+        PARSE_WRITE_ADDR,
+        PARSE_READ_ADDR,
+        PARSE_WRITE_DATA,
+        WAIT_FOR_ACK,
+        START,
+        STOP,
+        READ,
         OUTPUT
     } state_t;
     state_t state, next_state;
@@ -56,6 +61,7 @@ module ascii_command_decoder (
             cs <= 0;
             addr <= 16'd0;
             wdata <= 32'd0;
+            irq <= 1; //czy tu wystarczy?
         end else begin
             state <= next_state;
 
@@ -77,44 +83,90 @@ module ascii_command_decoder (
                 end
 
                 CHECK_CMD: begin
-                    if (command_reg[0] == "w" &&
+                    if (command_reg[0] == "s" &&
+                        command_reg[1] == "t" &&
+                        command_reg[2] == "a" &&
+                        command_reg[3] == "r" &&
+                        command_reg[4] == "t") begin
+                        next_state <= START;
+                    end
+                    else if (command_reg[0] == "s" &&
+                        command_reg[1] == "t" &&
+                        command_reg[2] == "o" &&
+                        command_reg[3] == "p") begin
+                        next_state <= STOP;
+                    end
+                    else if (command_reg[0] == "w" &&
                         command_reg[1] == "r" &&
                         command_reg[2] == "i" &&
                         command_reg[3] == "t" &&
                         command_reg[4] == "e" &&
                         command_reg[5] == " ") begin
-                        next_state <= PARSE_ADDR;
+                        next_state <= PARSE_WRITE_ADDR;
+                    end
+                    else if (command_reg[0] == "r" &&
+                             command_reg[1] == "e" &&
+                             command_reg[2] == "a" &&
+                             command_reg[3] == "d" &&
+                             command_reg[4] == " ") begin
+                             next_state <= PARSE_READ_ADDR; //parse read addr !
                     end else begin
                         next_state <= IDLE; 
                     end
                 end
 
-                PARSE_ADDR: begin
+                START: begin
+                    irq <= 0;
+                    next_state <= IDLE;
+                end
+                
+                STOP: begin
+                    irq <= 1;
+                    next_state <= IDLE;
+                end
+
+                PARSE_WRITE_ADDR: begin
                     if (is_hex_char(command_reg[6]) && is_hex_char(command_reg[7])) begin //tu jakas zmienna, [6] jest tylko dla write
                         parsed_addr <= {hex_to_bin(command_reg[6]), hex_to_bin(command_reg[7])};
-                        next_state <= PARSE_DATA;
+                        next_state <= PARSE_WRITE_DATA;
+                    end else begin
+                        next_state <= IDLE;  
+                    end
+                end
+                
+                PARSE_READ_ADDR: begin
+                    if (is_hex_char(command_reg[5]) && is_hex_char(command_reg[6])) begin //tu jakas zmienna, [6] jest tylko dla write
+                        parsed_addr <= {hex_to_bin(command_reg[5]), hex_to_bin(command_reg[6])};
+                        next_state <= READ;
                     end else begin
                         next_state <= IDLE;  
                     end
                 end
 
-                PARSE_DATA: begin
-                    if (is_hex_char(command_reg[8]) && is_hex_char(command_reg[9]) &&
-                        is_hex_char(command_reg[10]) && is_hex_char(command_reg[11]) &&
-                        is_hex_char(command_reg[12]) && is_hex_char(command_reg[13]) &&
-                        is_hex_char(command_reg[14]) && is_hex_char(command_reg[15])) begin
+                PARSE_WRITE_DATA: begin
+                    if (is_hex_char(command_reg[9]) && is_hex_char(command_reg[10]) &&
+                        is_hex_char(command_reg[11]) && is_hex_char(command_reg[12]) &&
+                        is_hex_char(command_reg[13]) && is_hex_char(command_reg[14]) &&
+                        is_hex_char(command_reg[15]) && is_hex_char(command_reg[16])) begin
                         parsed_data <= {
-                            hex_to_bin(command_reg[8]), hex_to_bin(command_reg[9]),
-                            hex_to_bin(command_reg[10]), hex_to_bin(command_reg[11]),
-                            hex_to_bin(command_reg[12]), hex_to_bin(command_reg[13]),
-                            hex_to_bin(command_reg[14]), hex_to_bin(command_reg[15])
+                            hex_to_bin(command_reg[9]), hex_to_bin(command_reg[10]),
+                            hex_to_bin(command_reg[11]), hex_to_bin(command_reg[12]),
+                            hex_to_bin(command_reg[13]), hex_to_bin(command_reg[14]),
+                            hex_to_bin(command_reg[15]), hex_to_bin(command_reg[16])
                         };
                         next_state <= OUTPUT;
                     end else begin
                         next_state <= IDLE;  // Niepoprawne dane\n
                     end
                 end
-
+                
+                WAIT_FOR_ACK: begin
+                    if (ack == 1) begin
+                        next_state <= IDLE;  // Transition to IDLE on ACK
+                    end else begin
+                        next_state <= WAIT_FOR_ACK;  // Stay in this state until ACK is received
+                    end
+                end
 
                 OUTPUT: begin
                     addr <= parsed_addr;
@@ -122,6 +174,14 @@ module ascii_command_decoder (
                     we <= 1;
                     cs <= 1;
                     next_state <= IDLE;
+                end
+                
+                READ: begin
+                    addr <= parsed_addr;
+                    wdata <= 32'd0;
+                    we <= 0;
+                    cs <= 1;
+                    next_state <= WAIT_FOR_ACK;
                 end
 
                 default: next_state <= IDLE;
